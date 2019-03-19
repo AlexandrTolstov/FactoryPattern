d8063997491d10c50c7a9573c9232daa75757c89
AlexandrTolstov <alextav_85@mail.ru>
2019-03-19 11:45:15 +0600

    1

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -10,6 +10,55 @@ namespace FactoryPattern
     {
         static void Main(string[] args)
         {
+            Developer dev = new PanelDeveloper("ООО КирпичСтрой");
+            House house2 = dev.Create();
+
+            dev = new WoodDeveloper("Частный застройщик");
+            House house = dev.Create();
+        }
+        //Абстрактный класс строительной компании
+        abstract class Developer
+        {
+            public string Name { get; set; }
+            public Developer(string n)
+            {
+                Name = n;
+            }
+            //фабричный метод
+            abstract public House Create();
+        }
+        abstract class House { }
+        class PanelHouse : House
+        {
+            public PanelHouse()
+            {
+                Console.WriteLine("Панельный дом построен");
+            }
+        }
+        class WoodHouse : House
+        {
+            public WoodHouse()
+            {
+                Console.WriteLine("Деревянный дом построен");
+            }
+        }
+        //Строит панельные дома
+        class PanelDeveloper : Developer
+        {
+            public PanelDeveloper(string n) : base(n) { }
+            public override House Create()
+            {
+                return new PanelHouse();
+            }
+        }
+        //Строит деревянные дома
+        class WoodDeveloper : Developer
+        {
+            public WoodDeveloper(string n) : base(n) { }
+            public override House Create()
+            {
+                return new WoodHouse();
+            }
         }
     }
 }
